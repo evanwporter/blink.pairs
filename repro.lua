d0f00431105f773ae50cdc0fa03208448f5f829e
sergentbne <75549567+sergentbne@users.noreply.github.com>
2026-06-19 10:47:46 -0400

    fix: fixed repro.lua (#115)
    ---------
    
    Co-authored-by: Liam Dyer <liamcdyer@gmail.com>

--- repro.lua
+++ repro.lua
@@ -2,4 +2,5 @@ vim.pack.add({
   'https://github.com/saghen/blink.download',
   { src = 'https://github.com/saghen/blink.pairs', version = vim.version.range('*') },
 })
-require('blink.pairs').setup({})
+require('blink.pairs').build():pwait(60000)
+require('blink.pairs').setup()
